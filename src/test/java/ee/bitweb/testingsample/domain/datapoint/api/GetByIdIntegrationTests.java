2d1edbbf9f00aaec4a69ef717e821eca61c3e882
SanderPaasalu <sander.paasalu@khk.ee>
2022-02-17 20:59:39 +0200

    4 tests that had to be done by 17.02.2022

--- src/test/java/ee/bitweb/testingsample/domain/datapoint/api/GetByIdIntegrationTests.java
+++ src/test/java/ee/bitweb/testingsample/domain/datapoint/api/GetByIdIntegrationTests.java
@@ -1,6 +1,7 @@
 package ee.bitweb.testingsample.domain.datapoint.api;
 
 import ee.bitweb.testingsample.common.trace.TraceIdCustomizerImpl;
+import ee.bitweb.testingsample.domain.datapoint.common.DataPoint;
 import ee.bitweb.testingsample.domain.datapoint.common.DataPointRepository;
 
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.*;
 import org.springframework.transaction.annotation.Transactional;
+
+import static ee.bitweb.testingsample.domain.datapoint.DataPointHelper.create;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,9 +37,65 @@ class GetByIdIntegrationTests {
     @Test
     @Transactional
     void onValidIdShouldReturnSuccessResponse() throws Exception {
+        DataPoint point = repository.save(create(1L));
+        mockMvc.perform(createDefaultRequest("1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", aMapWithSize(5)))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.externalId", is("external-id-1")))
+                .andExpect(jsonPath("$.value", is("some-value-1")))
+                .andExpect(jsonPath("$.comment", is("some-comment-1")))
+                .andExpect(jsonPath("$.significance", is(1)));
 
     }
 
+    @Test
+    @Transactional
+    void OnInvalidShouldReturnNotFound() throws  Exception {
+        DataPoint point = repository.save(create(1L));
+        mockMvc.perform(createDefaultRequest("3"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$", aMapWithSize(4)))
+                .andExpect(jsonPath("$.id", is(startsWith("ThisIsARequestId_"))))
+                .andExpect(jsonPath("$.message", is("Entity DataPoint not found")))
+                .andExpect(jsonPath("$.entity", is("DataPoint")))
+                .andExpect(jsonPath("$.criteria[0].field", is("Id")))
+                .andExpect(jsonPath("$.criteria[0].value", is(3)));
+    }
+
+    @Test
+    @Transactional
+    void onInvalidNegativeIdShouldReturnNotFound() throws Exception {
+        DataPoint point = repository.save(create(-1L));
+        mockMvc.perform(createDefaultRequest("3"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$", aMapWithSize(4)))
+                .andExpect(jsonPath("$.id", is(startsWith("ThisIsARequestId_"))))
+                .andExpect(jsonPath("$.message", is("Entity DataPoint not found")))
+                .andExpect(jsonPath("$.entity", is("DataPoint")))
+                .andExpect(jsonPath("$.criteria[0].field", is("id")))
+                .andExpect(jsonPath("$.criteria[0].value", is("3")));
+    }
+
+    @Test
+    @Transactional
+    void onMalformedIdShouldReturnBadrequest() throws Exception {
+        MockHttpServletRequestBuilder mockMvcBuilder = post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(TraceIdCustomizerImpl.DEFAULT_HEADER_NAME, REQUEST_ID)
+                .content("46fdgr");
+
+        mockMvc.perform(mockMvcBuilder)
+                .andDo((print()))
+                .andExpect(jsonPath("$", aMapWithSize(2)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.id", is(startsWith(REQUEST_ID))))
+                .andExpect(jsonPath("$.message", is("MESSAGE_NOT_READABLE")));
+    }
     private MockHttpServletRequestBuilder createDefaultRequest(String param) {
         return get(URI + "/" + param)
                 .contentType(MediaType.APPLICATION_JSON)
